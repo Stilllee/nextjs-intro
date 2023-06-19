904856cfd4e9b1089335f7272346a05994a31e47
Stilllee <orodae@gmail.com>
2023-06-20 00:47:00 +0900

    #2.6 Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,44 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 // _app.js의 pageProps를 통해 getServerSideProps함수가 Home의 props처럼 (results) 들어오게 됨
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.poster_path,
+                },
+              }}
+            >
+              {movie.original_title}
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -2,6 +2,11 @@ import { useRouter } from "next/router";
 
 export default function Detail() {
   const router = useRouter();
-  console.log(router);
-  return "detail";
+  return (
+    <div>
+      {/* router.query.title은 유저가 홈페이지에서 상세페이지로 넘어올때에만 존재 */}
+      {/* 그래서 시크릿 모드로 들어가면 Loading...이 뜸 */}
+      <h4>{router.query.title || "Loading..."}</h4>
+    </div>
+  );
 }
